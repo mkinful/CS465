ac7b8dd92c3fce29ce5437f7a6aaa0cb1765ebce
mkinful <68573464+mkinful@users.noreply.github.com>
2024-09-29 12:18:21 -0400

    Add files via upload

---
+++ travlr/app_server/controllers/travel.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+const travel = (req, res) => {
+  pageTitle = process.env.npm_package_description + ' - Travel';
+  res.render('travel', { title: pageTitle });
+};
+
+module.exports = {
+  travel,
+};
